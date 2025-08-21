ab84875d16189f6370947a3a87a5d5fe74355264
PedroHGGomes <pedro.gomes.10@outlook.com.br>
2025-08-21 14:46:45 -0300

    Atualização 2 - Mudanã Application

---
+++ demo/src/main/java/br/com/fiap/epictaska/CharacterWebappApplication.java
@@ -0,0 +1,11 @@
+package br.com.fiap.epictaska;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterWebappApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CharacterWebappApplication.class, args);
+    }
+}
